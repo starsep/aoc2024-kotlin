af4dac97aacba1033d5db55c24484ff3da7afa61
Filip Czaplicki <github@starsep.com>
2024-12-03 00:07:59 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val left = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
+        for (row in input) {
+            val parts = row.split("   ")
+            left.add(parts[0].toInt())
+            right.add(parts[1].toInt())
+        }
+        return left.sorted().zip(right.sorted()).sumBy { (a, b) ->
+            Math.abs(a - b)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val left = mutableListOf<Int>()
+        val countsB = mutableMapOf<Int, Int>()
+        for (row in input) {
+            val parts = row.split("   ")
+            left.add(parts[0].toInt())
+            val b = parts[1].toInt()
+            if (b !in countsB) {
+                countsB[b] = 0
+            }
+            countsB[b] = countsB.getOrDefault(b, 0) + 1
+        }
+        return left.sumBy {
+            it * (countsB.getOrDefault(it, 0))
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
